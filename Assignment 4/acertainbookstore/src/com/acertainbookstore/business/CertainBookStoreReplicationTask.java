038c28813b2691576a64efcc239d335ffe0eb1fa
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2014-12-11 13:50:01 +0100

    ReplicatorTask

--- Assignment 4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicationTask.java
+++ Assignment 4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicationTask.java
@@ -1,5 +1,13 @@
 package com.acertainbookstore.business;
 
+import com.acertainbookstore.utils.BookStoreException;
+import com.acertainbookstore.utils.BookStoreMessageTag;
+import com.acertainbookstore.utils.BookStoreUtility;
+import org.eclipse.jetty.client.ContentExchange;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.io.Buffer;
+import org.eclipse.jetty.io.ByteArrayBuffer;
+
 import java.util.concurrent.Callable;
 
 /**
@@ -9,14 +17,39 @@ import java.util.concurrent.Callable;
 public class CertainBookStoreReplicationTask implements
 		Callable<ReplicationResult> {
 
-	public CertainBookStoreReplicationTask() {
-		// TODO Auto-generated constructor stub
+    private ReplicationRequest request;
+    private String server;
+
+	public CertainBookStoreReplicationTask(String server, ReplicationRequest request) {
+        this.server = server;
+		this.request = request;
 	}
 
 	@Override
 	public ReplicationResult call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        System.out.println("FOOBAR");
+        ContentExchange exchange = new ContentExchange();
+        String urlString = server + BookStoreMessageTag.REPLICATEREQUEST;
+        System.out.println("URL: " + urlString);
+        exchange.setMethod("POST");
+        exchange.setURL(urlString);
+
+        String requestXml = BookStoreUtility.serializeObjectToXMLString(request);
+        Buffer requestContent = new ByteArrayBuffer(requestXml);
+        exchange.setRequestContent(requestContent);
+
+        try {
+            HttpClient client = new HttpClient();
+            client.start();
+            BookStoreUtility.SendAndRecv(client, exchange);
+            System.out.println("FOOBAR1");
+        } catch (BookStoreException ignore) {
+            System.out.println("FOOBAR2");
+            return new ReplicationResult(server, false);
+        } finally {
+            System.out.println("FOOBAR3");
+        }
+        return new ReplicationResult(server, true);
+    }
 
 }
